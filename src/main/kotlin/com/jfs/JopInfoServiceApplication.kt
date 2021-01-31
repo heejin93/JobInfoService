d9ee8948b7218caa1f8c74b28ff5c1c767e4e604
김희진 <ww01095013411@gmail.com>
2021-01-31 15:34:28 +0900

    apllicaiton 및 build.gradle 설정

---
+++ src/main/kotlin/com/jfs/JopInfoServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.jfs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JopInfoServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<JopInfoServiceApplication>(*args)
+}
